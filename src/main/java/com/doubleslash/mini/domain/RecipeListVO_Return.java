d2ae6121fa7340076dc9cc0919dc485be44d0150
iustron@naver.com <iustron@naver.com>
2020-09-30 22:11:06 +0900

    [Add] Return VO 추가

---
+++ src/main/java/com/doubleslash/mini/domain/RecipeListVO_Return.java
@@ -0,0 +1,15 @@
+package com.doubleslash.mini.domain;
+
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RecipeListVO_Return {
+		
+	private List<RecipeListVO> menu_list;
+	
+}
